a93b8c8a623ca0c522e84af9c5b40dc1ef03b6f8
ahaow <ahao72@qq.com>
2020-11-05 17:59:44 +0800

    wh

--- src/views/touch.vue
+++ src/views/touch.vue
@@ -1,11 +1,12 @@
 <template>
-  <div>
+  <div class="touch" id="touch">
     <canvas id="ccc" width="1920" height="1080"></canvas>
   </div>
 </template>
 
 <script>
 import Fabric from "fabric";
+import Hammer from "hammerjs";
 export default {
   name: "touch",
   data() {
@@ -13,7 +14,19 @@ export default {
       imgUrl: require("../assets/images/31.png"),
     };
   },
+  methods: {
+    initTouch() {
+      const touchEle = document.getElementById("touch");
+      const hammer = new Hammer(touchEle);
+      console.log(hammer)
+      hammer.on("pan", function (e) {
+        console.log("You're pressing me!");
+        console.log(e);
+      });
+    },
+  },
   mounted() {
+    this.initTouch();
     var canvas = new fabric.Canvas("ccc");
     fabric.Image.fromURL(this.imgUrl, function (img) {
       img.scale(0.5).set({
